b6a533b93337c963c925ad697c45f3a9eba7d309
Gene Hallman <genehallman@gmail.com>
2013-05-09 14:51:39 -0600

    Update example.js

--- example.js
+++ example.js
@@ -2,13 +2,19 @@ define(function(require) {
 	var Hub = require('streamhub-sdk');
 	var View = require('streamhub-timeline');
 
-	return function(sdk, opts) {
+	return function(el) {
 
-        var view = new View({
-            streams: Hub.Streams.forCollection(opts).start(),
-            el: document.getElementById("timelineHolder")
-        });
-        
-        return view;
-    };
+	        var view = new View({ el: el });
+	
+		var streams = Hub.StreamManager.create.livefyreStreams({
+		    network: "labs-t402.fyre.co",
+		    environment: "t402.livefyre.com",
+		    siteId: "303827",
+		    articleId: 'labs_demo_fire'
+		});
+		
+		streams.bind(view).start();
+		
+	        return view;
+	};
 });
