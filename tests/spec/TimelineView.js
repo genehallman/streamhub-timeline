079a1c0a6c44d55f47991804418d0049f4068aee
Gene Hallman <gene@livefyre.com>
2013-05-16 13:29:15 -0700

    fix tests for app gallery

--- tests/spec/TimelineView.js
+++ tests/spec/TimelineView.js
@@ -2,21 +2,9 @@ define([
     'jasmine-jquery',
     'streamhub-timeline',
     'streamhub-sdk',
-    'streamhub-sdk/streams',
     '../MockStream'],
-function (jasmine, TimelineView, Hub, Streams, MockStream) {
+function (jasmine, TimelineView, Hub, MockStream) {
 describe('A TimelineView', function () {
-    it ("can have tests run", function () {
-        expect(true).toBe(true);
-    });
-    it("can do HTML tests",function(){  
-        setFixtures('<div id="hub"></div>');  
-        $('#hub')
-            .append('<li>So</li>')
-            .append('<li>So</li>');
-        expect($('#hub li').length).toBe(2);  
-    });
-	
 	// construction behavior
     describe('can be constructed', function() {
     	it ("with no options", function () {
@@ -27,12 +15,6 @@ describe('A TimelineView', function () {
         	var view = new TimelineView({});
         	expect(view).toBeDefined();
     	});
-    	it ("with only a Mock Hub.Stream", function () {
-        	var view = new TimelineView({
-            	streams: new Streams({main: new MockStream()})
-        	});
-    	    expect(view).toBeDefined();
-    	});
 	    it ("with an el", function () {
 	        setFixtures('<div id="hub-TimelineView"></div>');  
 	        var view = new TimelineView({
@@ -40,32 +22,24 @@ describe('A TimelineView', function () {
 	        });
 	        expect(view).toBeDefined();
 	    });
-	    it ("with an el and Mock Hub.Stream", function () {
-	        setFixtures('<div id="hub-TimelineView"></div>');  
-	        var view = new TimelineView({
-	            streams: new Streams({main: new MockStream()}),
-	            el: $('#hub-TimelineView').get(0)
-	        });
-	        expect(view).toBeDefined();
-	    });
 	});
 	
 	// post construction behavior    
-    describe ("after correct construction", function () {
-	    var view;
+    describe ("after correct construction and binding", function () {
+	    var view, streams;
 	    
 	    beforeEach(function() {
-	        setFixtures(
-		        '<div id="hub-TimelineView"></div>'
-		    );
+	        setFixtures('<div id="hub-TimelineView"></div>');
 	        view = new TimelineView({
-	            streams: new Streams({main: new MockStream()}),
 	            el: $('#hub-TimelineView').get(0)
 	        });
-	        window.view = view;
+	        console.log('here 1');
+	        streams = new Hub.StreamManager({ main: new MockStream() });
+	        console.log('here 2');
+            streams.bind(view);
 		});
         it ("should contain 50 mock items after streams.start()", function () {
-            view.streams.start();
+            streams.start();
             expect(view.$el.find('.events ul>li').length).toBe(50);
         });
         it ("should call function pointer on click", function () {
@@ -73,16 +47,16 @@ describe('A TimelineView', function () {
         	view.onClick = function() {
         		counter++;
         	};
-        	view.streams.start();
+            streams.start();
         	view.$el.find('.events ul>li').get(20).click();
             expect(counter).toBe(1);
         });
         it ("should filter on start and end dates", function () {
-        	var content = [view.streams.get('main')._createMockContent(),
-	        	view.streams.get('main')._createMockContent(),
-	        	view.streams.get('main')._createMockContent(),
-	        	view.streams.get('main')._createMockContent(),
-	        	view.streams.get('main')._createMockContent()];
+        	var content = [streams.get('main')._createMockContent(),
+	        	streams.get('main')._createMockContent(),
+	        	streams.get('main')._createMockContent(),
+	        	streams.get('main')._createMockContent(),
+	        	streams.get('main')._createMockContent()];
 	        
         	view.startDate = content[1].createdAt * 1000;
         	view.endDate = content[3].createdAt * 1000;
@@ -92,7 +66,7 @@ describe('A TimelineView', function () {
             expect(view.$el.find('.events ul>li').length).toBe(3);
         });
         it ("should pass extra meta content", function () {
-        	var content = view.streams.get('main')._createMockContent();
+        	var content = streams.get('main')._createMockContent();
         	content.body = '<p>test</p><em>{"eventType":"testType", "eventImportant":"true"}</em>';
 
         	view.metaElement = 'em';
